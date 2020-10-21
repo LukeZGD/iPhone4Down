2abfa7f42bc981a98468b71980bd145f8fbef9e7
LukeeGD <l32luke@gmail.com>
2020-10-21 21:14:55 +0800

    Update idevicerestore macOS

--- restore.sh
+++ restore.sh
@@ -584,6 +584,8 @@ function InstallDependencies {
         # macOS
         xcode-select --install
         SaveFile https://github.com/libimobiledevice-win32/imobiledevice-net/releases/download/v1.3.6/libimobiledevice.1.2.1-r1091-osx-x64.zip libimobiledevice.zip dba9ca5399e9ff7e39f0062d63753d1a0c749224
+        Log "(Enter root password of your Mac when prompted)"
+        sudo codesign --sign - --force --deep ../resources/tools/idevicerestore_macos
         
     else
         Error "Distro not detected/supported by the install script." "See the repo README for supported OS versions/distros"
